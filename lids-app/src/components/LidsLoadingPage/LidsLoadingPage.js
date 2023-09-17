691a2b72b5106e81ecf9c34a6ca87effcadf8990
Carlos A <cmalcazar@miners.utep.edu>
2023-09-16 20:20:24 -0600

    Changes
    replaced gif, added CSS file to match Ruth UI, modified LoadingPage

--- lids-app/src/components/LidsLoadingPage/LidsLoadingPage.js
+++ lids-app/src/components/LidsLoadingPage/LidsLoadingPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import logo from './loading_gif.gif'
+import './LidsLoadingPage.css';
 
 function LidsLoadingPage({ onEnterPress }) {
   useEffect(() => {
@@ -17,23 +18,19 @@ function LidsLoadingPage({ onEnterPress }) {
     };
   }, [onEnterPress]);
 
-function goHome() {
-  alert("going home"); //?
-}
-
-function goBack() {
-  alert('You clicked me!'); //?
-}
-
   return (
     <div className="lids-loading-page">
-      <button style = {{backgroundColor : "LightGray", padding: "10px 60px", borderRadius: "5px", margin: "0px 1px"}}onClick={goHome}>Main Menu</button>
-      <button style = {{backgroundColor : "LightGray", padding: "10px 60px", borderRadius: "5px", margin: "0px 0px"}}onClick={goBack}>Go Back</button>
-      <h1 style = {{color: "white", textAlign: "center", backgroundColor: "#07172C"}}>LIDS</h1>
+      <div className="top-section">
+      <button style = {{backgroundColor : "LightGray", padding: "10px 60px", borderRadius: "5px", margin: "0px 1px"}}onClick={onEnterPress}>Main Menu</button>
+      <button style = {{backgroundColor : "LightGray", padding: "10px 60px", borderRadius: "5px", margin: "0px 0px"}}onClick={onEnterPress}>Go Back</button>
+      <h1 className="h1-custom">LIDS</h1>
+      </div>
       <br></br>
+      <div className="bottom-section">
       <h2 style = {{color: "white", textAlign: "center", }}>Loading Configuration File...</h2>
       <img  src = {logo} alt = "loading.." style = {{display: "block", margin: "0 auto"}}/>
-      <h2 style = {{position: "fixed", left: "44%", bottom: "0" , color: "white",textAlign: "center", backgroundColor: "#07172C"}}>LIDS 000.000.00.000</h2> {/* Instead of 0s, get actual IP*/}
+      <h2 style = {{position: "fixed", left: "44%", bottom: "0" , color: "white",textAlign: "center", backgroundColor: "#11253D"}}>LIDS 000.000.00.000</h2> {/* Instead of 0s, get actual IP*/}
+      </div>
     </div>
   );
 }
