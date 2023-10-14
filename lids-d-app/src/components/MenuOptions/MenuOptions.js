9944b15ae85bf57432e18f9cc2dbfaeb355d4691
jshoe54 <jlshoemaker_8@live.com>
2023-10-14 15:08:30 -0600

    Added NetworkMap and Routing for LIDS-D GUI

--- lids-d-app/src/components/MenuOptions/MenuOptions.js
+++ lids-d-app/src/components/MenuOptions/MenuOptions.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MenuOptions extends React.Component {
   render() {
     return (
       <div className="menu-options-container">
-        <div className="menu-box">Malicious packet count</div>
-        <div className="menu-box">Configure Server</div>
-        <div className="menu-box">View Alerts</div>
-        <div className="menu-box">Network information</div>
+        <div className="menu-box">Malicious packet count</div> {/* If this needs linking later, you can add a route */}
+        
+        <Link to="/configure-server" className="menu-box button-style">Configure Server</Link>
+        <Link to="/view-alerts" className="menu-box button-style">View Alerts</Link>
+        <Link to="/network-information" className="menu-box button-style">Network information</Link>
       </div>
     );
   }
