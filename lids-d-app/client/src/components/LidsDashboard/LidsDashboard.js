c5ec4f4f2b15fb2d85b13e1e48905af950e63992
Hope1002 <reavilarodr@utep.edu>
2023-10-31 00:32:06 -0600

    Added modifiers to files

--- lids-d-app/client/src/components/LidsDashboard/LidsDashboard.js
+++ lids-d-app/client/src/components/LidsDashboard/LidsDashboard.js
@@ -2,6 +2,9 @@
  * @author X
  * @version 1.0, 05/05/23
 */
+/**
+ * @modifiers
+ */
 
 import React, { useEffect, useState } from 'react';
 import SortByDropdown from '../SortByDropdown/SortByDropdown';
