168c686edb4e7b6594191f19f9d1be202ef75c18
Bmadri02 <bamadrigal@miners.utep.edu>
2023-11-12 21:09:34 -0700

    updating network map

--- lids-d-app/client/src/components/NetworkInformation/NetworkMap.js
+++ lids-d-app/client/src/components/NetworkInformation/NetworkMap.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import Windows_known from './lids-d-app/client/src/components/NetworkInformation/apple-logo-known.png';
+import Windows_unknown from './lids-d-app/client/src/components/NetworkInformation/windows-logo-unknown.png';
+import Linux_known from'./lids-d-app/client/src/components/NetworkInformation/linux-logo-known.png';
+import Linux_unknown from './lids-d-app/client/src/components/NetworkInformation/linux-logo-unknown.png'; 
+import Mac_known from './lids-d-app/client/src/components/NetworkInformation/apple-logo-known.png'; 
+import Mac_unknown from './lids-d-app/client/src/components/NetworkInformation/apple-logo-unknown.png'; 
 
 function DeviceNode({ device, destIp }) {
   const color = device.status === 'recognized' ? 'green' : 'red';
@@ -23,27 +29,53 @@ function DeviceNode({ device, destIp }) {
     </g>
   );
 }
+const imageData = {
+  id: 1,
+  name: "Device 1",
+  status: "recognized",
+  x: 50,
+  y: 50,
+  imagePath: getOsImage('Windows_known'),
+  id: 2, name: "Device 2", status: "unknown", x: 100, y: 100, imagePath: getOsImage('Linux_unknown'),
+  id: 3, name: "Device 3", status: "recognized", x: 150, y: 150, imagePath: getOsImage('Mac_known')
+};
 
-function getOsImage(os) {
-
-  const osImages = {
-    'Windows_known': 'lids-d-app/client/src/components/NetworkInformation/apple-logo-known.png',
-    'Windows_unknown': 'lids-d-app/client/src/components/NetworkInformation/windows-logo-unknown.png',
-    'Linux_known': 'lids-d-app/client/src/components/NetworkInformation/linux-logo-known.png',
-    'Linux_unknown': 'lids-d-app/client/src/components/NetworkInformation/linux-logo-unknown.png',
-    'Mac_known': 'lids-d-app/client/src/components/NetworkInformation/apple-logo-known.png',
-    'Mac_unknown': 'lids-d-app/client/src/components/NetworkInformation/apple-logo-unknown.png',
-  };
-
-  return osImages[os];
+  const getOsImage = (osLogos) => {
+        switch(osLogos) {
+            case 'Windows_known' :
+              return Windows_known; 
+            case 'Windows_uknknown' :
+              return Windows_unknown;    
+            case 'Linux_known' :
+                return Linux_known; 
+            case 'Linux_uknknown' :
+              return Linux_unknown; 
+            case 'Mac_known' :
+                return Mac_known; 
+            case 'Mac_uknknown' :
+              return Mac_unknown; 
+          default:
+              return null;
+      }
+};
+function NetworkMap(){ 
+  return(
+    <div style={{ position: 'relative', left: imageData.x, top: imageData.y }}>
+      <img
+        src={imageData.imagePath}
+        alt={'${imageData.name}, status: ${imageData.status}'}
+        title={'ID: ${imageData.id}, Name: ${imageData.name}, Status: ${imageData.status}'}
+      />
+      <div className="image-info">
+        <p>ID: {imageData.id}</p>
+        <p>Name: {imageData.name}</p>
+        <p>Status: {imageData.status}</p>
+        <p>Coordinates: ({imageData.x}, {imageData.y})</p>
+      </div>
+    </div>
+    
+  );
 }
-
-function NetworkMap() {
-  const [devices, setDevices] = useState([
-    { id: 1, name: "Device 1", status: "recognized", x: 50, y: 50, os: 'Windows_known' },
-    { id: 2, name: "Device 2", status: "unknown", x: 100, y: 100, os: 'Linux_unknown' },
-    { id: 3, name: "Device 3", status: "recognized", x: 150, y: 150, os: 'Mac_known' },
-  ]);
   /*
 function NetworkMap() {
     const [devices, setDevices] = React.useState([]);
@@ -99,5 +131,6 @@ function NetworkMap() {
     </div>
   );
 }
+
 
 export default NetworkMap;
