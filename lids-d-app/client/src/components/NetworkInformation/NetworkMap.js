f9094a3bd3028218a2449b432c65ad0518380977
Bmadri02 <bamadrigal@miners.utep.edu>
2023-11-12 19:51:53 -0700

    updating network map

--- lids-d-app/client/src/components/NetworkInformation/NetworkMap.js
+++ lids-d-app/client/src/components/NetworkInformation/NetworkMap.js
@@ -1,29 +1,50 @@
-/**
- * @author X
- * @version 1.0, 05/05/23
-*/
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom'; // Added import
-//import * as d3 from 'd3';
+import { useNavigate } from 'react-router-dom';
 
-function DeviceNode({ device }) {
-    const color = device.status === 'recognized' ? 'green' : 'red';
-    return (
-        
-        <circle cx={device.x} cy={device.y} r={10} fill={color} />
-    );
+function DeviceNode({ device, destIp }) {
+  const color = device.status === 'recognized' ? 'green' : 'red';
+  const osImage = getOsImage(device.os);
+
+  return (
+    <g>
+      <circle cx={device.x} cy={device.y} r={10} fill={color} />
+      {osImage && (
+        <image
+          x={device.x - 10}
+          y={device.y - 30} 
+          href={osImage}
+          width="20" 
+          height="20" 
+        />
+      )}
+      <text x={device.x} y={device.y + 15} textAnchor="middle">{destIp}</text>
+    </g>
+  );
+}
+
+function getOsImage(os) {
+
+  const osImages = {
+    'Windows_known': 'lids-d-app/client/src/components/NetworkInformation/apple-logo-known.png',
+    'Windows_unknown': 'lids-d-app/client/src/components/NetworkInformation/windows-logo-unknown.png',
+    'Linux_known': 'lids-d-app/client/src/components/NetworkInformation/linux-logo-known.png',
+    'Linux_unknown': 'lids-d-app/client/src/components/NetworkInformation/linux-logo-unknown.png',
+    'Mac_known': 'lids-d-app/client/src/components/NetworkInformation/apple-logo-known.png',
+    'Mac_unknown': 'lids-d-app/client/src/components/NetworkInformation/apple-logo-unknown.png',
+  };
+
+  return osImages[os];
 }
 
 function NetworkMap() {
-    // For simplicity, we're using static data here.
-    const [devices, setDevices] = React.useState([
-        { id: 1, name: "Device 1", status: "recognized", x: 50, y: 50 },
-        { id: 2, name: "Device 2", status: "unknown", x: 100, y: 100 },
-        { id: 3, name: "Device 3", status: "recognized", x: 150, y: 150 },
-    ]);
-/*
+  const [devices, setDevices] = useState([
+    { id: 1, name: "Device 1", status: "recognized", x: 50, y: 50, os: 'Windows_known' },
+    { id: 2, name: "Device 2", status: "unknown", x: 100, y: 100, os: 'Linux_unknown' },
+    { id: 3, name: "Device 3", status: "recognized", x: 150, y: 150, os: 'Mac_known' },
+  ]);
+  /*
 function NetworkMap() {
     const [devices, setDevices] = React.useState([]);
 
@@ -36,36 +57,47 @@ function NetworkMap() {
             });
     }, []);
     */
-    const navigate = useNavigate(); // Added hook
+  const [destIpData, setDestIpData] = useState([]);
+  const navigate = useNavigate();
 
-    const handleConfigureServer = () => { // Added function
-        navigate('/config-server');
-      };
-    
-      const handleViewAlerts = () => {
-        navigate('/view-alerts')
-      };
-      
-      const handleNetworkInfo = () => {
-        navigate('/network-map')
-      };
+  useEffect(() => {
+    Axios.get('http://127.0.0.1:5000/getAlertsIP')
+      .then((response) => {
+        const destIpData = response.data.map(item => item.dest_ip);
+        setDestIpData(destIpData);
+      })
+      .catch((error) => {
+        console.error("Error fetching dest_ip data:", error);
+      });
+  }, []);
 
-    return (
-        <div>
-            <button className="go-back-button" onClick={handleConfigureServer}>Configure Server</button>
-            <button className="go-back-button" onClick={handleViewAlerts}>View Alerts</button>
-            <button className="go-back-button" onClick={handleNetworkInfo}>Network Information</button>
-            <svg width="400" height="400">
-                {devices.map(device => (
-                    <DeviceNode key={device.id} device={device} />
-                ))}
-            </svg>
+  const handleConfigureServer = () => {
+    navigate('/config-server');
+  };
 
-            <Link to="/network-info">
-                <button>Network Info</button>
-            </Link>
-        </div>
-    );
+  const handleViewAlerts = () => {
+    navigate('/view-alerts');
+  };
+
+  const handleNetworkInfo = () => {
+    navigate('/network-map');
+  };
+
+  return (
+    <div>
+      <button className="go-back-button" onClick={handleConfigureServer}>Configure Server</button>
+      <button className="go-back-button" onClick={handleViewAlerts}>View Alerts</button>
+      <button className="go-back-button" onClick={handleNetworkInfo}>Network Information</button>
+      <svg width="400" height="400">
+        {devices.map((device, index) => (
+          <DeviceNode key={device.id} device={device} destIp={destIpData[index]} />
+        ))}
+      </svg>
+      <Link to="/network-info">
+        <button>Network Info</button>
+      </Link>
+    </div>
+  );
 }
 
 export default NetworkMap;
