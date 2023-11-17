f6d3f4dd34dc65bf3a4e11dcf0b36cd66a4fa695
Bmadri02 <bamadrigal@miners.utep.edu>
2023-11-17 00:40:04 -0700

    Updating Network map to reflect client example

--- lids-d-app/client/src/components/NetworkInformation/NetworkMap.js
+++ lids-d-app/client/src/components/NetworkInformation/NetworkMap.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 
-import Windows_known from './lids-d-app/client/src/components/NetworkInformation/windows-logo-known.png';
-import Windows_unknown from './lids-d-app/client/src/components/NetworkInformation/windows-logo-unknown.png';
-import Linux_known from './lids-d-app/client/src/components/NetworkInformation/linux-logo-known.png';
-import Linux_unknown from './lids-d-app/client/src/components/NetworkInformation/linux-logo-unknown.png'; 
-import Mac_known from './lids-d-app/client/src/components/NetworkInformation/apple-logo-known.png'; 
-import Mac_unknown from './lids-d-app/client/src/components/NetworkInformation/apple-logo-unknown.png'; 
+import Windows_known from './microsoft-logo-known.png';
+import Windows_unknown from './windows-logo-unknown.png';
+import Linux_known from './linux-logo-known.png';
+import Linux_unknown from './linux-logo-unknown.png'; 
+import Mac_known from './apple-logo-known.png'; 
+import Mac_unknown from './apple-logo-unknown.png';
 
 const getOsImage = (osName, status) => {
   const osStatus = status === 'recognized' ? 'known' : 'unknown';
@@ -31,17 +31,30 @@ const getOsImage = (osName, status) => {
 
 const DeviceNode = ({ device, destIp }) => {
   const osImage = getOsImage(device.os, device.status);
+  const rectWidth = 150;
+  const rectHeight = 180; 
+  const padding = 10;
 
   return (
     <g>
-      <circle cx={device.x} cy={device.y} r={10} fill={device.status === 'recognized' ? 'green' : 'red'} />
+      <rect
+        x={device.x - rectWidth / 2}
+        y={device.y - rectHeight / 2}
+        width={rectWidth}
+        height={rectHeight}
+        fill="none"
+        stroke="black"
+        strokeWidth="2"
+      />
+      <text x={device.x} y={device.y - rectHeight / 2 + padding} textAnchor="middle">Host:{device.host}</text>
+      <circle cx={device.x} cy={device.y - padding} r={10} fill={device.status === 'recognized' ? 'green' : 'red'} />
       {osImage && (
         <image
-          x={device.x - 10}
-          y={device.y - 30}
+          x={device.x - rectWidth / 4}
+          y={device.y - rectHeight / 2 + padding}
           href={osImage}
-          width="20"
-          height="20"
+          width="45"
+          height="45"
         />
       )}
       <text x={device.x} y={device.y + 15} textAnchor="middle">{destIp}</text>
@@ -50,7 +63,11 @@ const DeviceNode = ({ device, destIp }) => {
 };
 
 function NetworkMap() {
-  const [devices, setDevices] = useState([]);
+  const [devices, setDevices] = useState([
+    { id: 1, os: 'Windows', status: 'recognized', x: 150, y: 100, host: "Diana"},
+    { id: 2, os: 'Linux', status: 'unknown', x: 300, y: 150, Host:"unknown"},
+    { id: 3, os: 'Mac', status: 'recognized', x: 450, y: 175, Host:"Sebastian"}
+  ]);
   const [destIpData, setDestIpData] = useState([]);
   const navigate = useNavigate();
 
@@ -78,7 +95,7 @@ function NetworkMap() {
       <button className="go-back-button" onClick={handleConfigureServer}>Configure Server</button>
       <button className="go-back-button" onClick={handleViewAlerts}>View Alerts</button>
       <button className="go-back-button" onClick={handleNetworkInfo}>Network Information</button>
-      <svg width="400" height="400">
+      <svg width="1000" height="600">
         {devices.map((device, index) => (
           <DeviceNode key={device.id} device={device} destIp={destIpData[index]} />
         ))}
